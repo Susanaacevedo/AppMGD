e363a4598bccf7977a08e09df264833212f22606
Susana <sacevedo.jimenez@gmail.com>
2019-11-23 11:58:18 -0300

    Pasando el modelo a Rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191116153855) do
+ActiveRecord::Schema.define(version: 20191123145341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carros", force: :cascade do |t|
+    t.integer "total"
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_carros_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +42,5 @@ ActiveRecord::Schema.define(version: 20191116153855) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carros", "users", column: "users_id"
 end
